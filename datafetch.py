08c3403845ddcf7941a93d266bcf746d05610989
Edwin Lim <edwinlzs97@gmail.com>
2021-04-01 10:56:24 +0800

    completed data fetching and database updating scripts

--- datafetch.py
+++ datafetch.py
@@ -6,41 +6,57 @@ import numpy as np
 
 historicals = {}
 
-start_date = "1991-01-01"
-end_date = "2021-01-01"
-interval = "1d"
-
-# yahoofinance data types available: open, high, low, close, volume, dividends, stock_splits
-# yahoofinance data available: S&P500, NASDAQ Composite, FTSE100,
-    # HSI, SZSE Composite, SSE Composite,
-    # Gold Futures, Silver Futures, Copper Futures, Crude Oil Futures
-    # Treasury Yield 10 Years
-
 from_yahoofinance = {"S&P500":"SPY", "NASDAQ Composite":"^IXIC", "FTSE100":"^FTSE", "Hang Seng Index":"^HSI",
-                    "SZSE Composite":"399106.SZ", "SSE Composite":"000001.SS",
-                    "Gold Futures":"GC=F", "Silver Futures":"SI=F", "Copper Futures":"HG=F", "Crude Oil Futures":"CL=F",
-                    "10y Treasury Yield":"^TNX"}
+                        # equities
+                        "SZSE Composite":"399106.SZ", "SSE Composite":"000001.SS",
+                        # bonds
+                        "10y Treasury Yield":"^TNX",
+                        # commodities: metals
+                        "Gold Futures":"GC=F", "Silver Futures":"SI=F", "Platinum Futures":"PL=F", "Palladium Futures":"PA=F", "Copper Futures":"HG=F",
+                        # commodities: energy
+                        "Crude Oil Futures":"CL=F", "Natural Gas Futures":"NG=F",
+                        # commodities: agriculture
+                        "Feeder Cattle Futures":"GF=F", "Live Cattle Futures":"LE=F", "Cocoa Futures":"CC=F", "Lumber Futures":"LBS=F",
+                        "Cotton Futures":"CT=F", "Wheat Futures":"ZW=F", "Corn Futures":"ZC=F", "Soybean Futures":"ZS=F", "Sugar #11 Futures":"SB=F",
+                        "Oat Futures":"ZO=F", "Rough Rice Futures":"ZR=F"
+                        }
 
-for name in from_yahoofinance:
-    print("fetching data for: " + name)
-    symbol = from_yahoofinance[name]
-    target = yf_object(symbol)
-    historicals[name] = (target.get_close(interval=interval, start_date=start_date, end_date=end_date))
-    print("success")
+from_fred = {"M2 Money Supply":"M2SL", "M2 Money Velocity":"M2V", "Federal Funds Rate":"FEDFUNDS",
+                "Monetary Base Total":"BOGMBASE", "Personal Consumption Expenditures":"PCEPI"}
 
-# FRED API key = b296451576f9df1503841719b3d24ea5
-# FRED data available: M2SL (M2 Money Supply), FEDFUNDS (Federal Funds Rate), M2V (M2 Velocity)
-    # BOGMBASE (Monetary Base Total)
-from_fred = {"M2 Money Supply":"M2SL", "M2 Velocity":"M2V", "Federal Funds Rate":"FEDFUNDS",
-            "Monetary Base Total":"BOGMBASE", "Personal Consumption Expenditures":"PCEPI"}
+def fetch_data(start_date, end_date, interval):
+    # yahoofinance data types available: open, high, low, close, volume, dividends, stock_splits
+    # yahoofinance data available: S&P500, NASDAQ Composite, FTSE100,
+        # HSI, SZSE Composite, SSE Composite,
+        # Treasury Yield 10 Years,
+        # Gold Futures, Silver Futures, Crude Oil Futures, Copper Futures,
 
-api_key = 'b296451576f9df1503841719b3d24ea5'
-fred = fred_object(api_key=api_key)
+    for name in from_yahoofinance:
+        print("fetching data for: " + name)
+        symbol = from_yahoofinance[name]
+        target = yf_object(symbol)
+        df = target.get_close(interval=interval, start_date=start_date, end_date=end_date)
+        df.rename(index='value',inplace=True) # standardization for storing in db
+        df.index.name = 'date'
+        historicals[name] = df
+        print("success.")
 
-for name in from_fred:
-    print("fetching data for: " + name)
-    symbol = from_fred[name]
-    historicals[name] = fred.get_historical(symbol)
-    print("success")
+    # FRED API key = b296451576f9df1503841719b3d24ea5
+    # FRED data available: M2SL (M2 Money Supply), FEDFUNDS (Federal Funds Rate), M2V (M2 Velocity)
+        # BOGMBASE (Monetary Base Total), PCEPI (Personal Consumption Expenditures)
 
-print("Script run complete.")
+    api_key = 'b296451576f9df1503841719b3d24ea5'
+    fred = fred_object(api_key=api_key)
+
+    for name in from_fred:
+        print("fetching data for: " + name)
+        symbol = from_fred[name]
+        df = fred.get_historical(symbol)
+        df.rename(index='value',inplace=True) # standardization for storing in db
+        df.index.name = 'date'
+        historicals[name] = df
+        print("success.")
+
+    print("Data fetch complete.")
+
+    return historicals
---
+++ updatedb.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+from datafetch import fetch_data
+
+Base = declarative_base()
+
+# create database engine
+engine = sqlalchemy.create_engine('mysql+mysqlconnector://root@localhost:3306/macrodata')
+
+# # initialize session
+# Session = sessionmaker()
+# Session.configure(bind=engine)
+
+# pull data
+start_date = "1991-01-01"
+end_date = "2021-01-01"
+interval = "1d"
+
+data = fetch_data(start_date=start_date, end_date=end_date, interval=interval)
+
+for name, df in data.items():
+    name = name.lower()
+    print("updating: " + name)
+    df.to_sql(name, engine, if_exists='append')
+    print("success.")
